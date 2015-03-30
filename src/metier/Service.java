2f17488fe535f5742d701738de76c85a92b08a06
AJC Formation CBarsan <AJC Formation@ajc-PC>
2015-03-30 14:37:55 +0200

    Initial commit

---
+++ src/metier/Service.java
@@ -0,0 +1,36 @@
+package metier;
+
+public class Service {
+	
+	private int id;
+	private String libelle;
+	private String interlocuteur;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLibelle() {
+		return libelle;
+	}
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+	public String getInterlocuteur() {
+		return interlocuteur;
+	}
+	public void setInterlocuteur(String interlocuteur) {
+		this.interlocuteur = interlocuteur;
+	}
+	public Service(int id, String libelle, String interlocuteur) {
+		super();
+		this.id = id;
+		this.libelle = libelle;
+		this.interlocuteur = interlocuteur;
+	}
+	
+	
+
+}
